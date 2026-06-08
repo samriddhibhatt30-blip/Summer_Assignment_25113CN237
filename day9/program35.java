7faf9c4a2e73c9dcb4d68866aa85afad1468af41
SAMRIDDHI BHATT <samriddhibhatt30@gmail.com>
2026-06-08 22:26:02 +0530

    day9 uploaoed

---
+++ day9/program35.java
@@ -0,0 +1,16 @@
+package day9;
+
+public class program35 {
+    public static void main(String[] args)
+    {
+        int n=5;
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=1;j<=i;j++)
+            {
+                System.out.print((char)('A'+i-1));
+            }
+            System.out.println();
+        }
+    }
+}
